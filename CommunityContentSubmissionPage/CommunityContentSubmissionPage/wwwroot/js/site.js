f66ac8c980d7e29fb41f76fc6124f04fe5e5a58e
Andreas Willich <andreas@willich.io>
2020-11-12 14:21:03 +0100

    load type values via ajax from server

--- CommunityContentSubmissionPage/CommunityContentSubmissionPage/wwwroot/js/site.js
+++ CommunityContentSubmissionPage/CommunityContentSubmissionPage/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: 'api/AvailableTypes',
+        success: function (result, status, message) {
+
+            var selectElement = $("#txtType")[0];
+
+            var listOfTypes = result.types;
+
+            for (var i = 0; i < listOfTypes.length; i++) {
+                var text = listOfTypes[i];
+
+                var option = document.createElement('option');
+                option.innerHTML = text;
+
+                if (text === result.selectedType) {
+                    option.selected = true;
+                }
+
+                selectElement.appendChild(option);
+            }
+
+        },
+        error: function (error, type) { }
+    });
+});
