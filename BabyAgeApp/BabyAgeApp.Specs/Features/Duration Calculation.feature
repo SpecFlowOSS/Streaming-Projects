a1cc3f563b02fed0e80f991ba3901beb6afa5ff2
Andreas Willich <andreas@willich.io>
2020-11-26 14:49:35 +0100

    add first scenario to test calculation

---
+++ BabyAgeApp/BabyAgeApp.Specs/Features/Duration Calculation.feature
@@ -0,0 +1,7 @@
+﻿Feature: Duration Calculation
+
+Scenario: Age in Month Calculation
+
+	Given the baby is born on '2020-08-18 12:56:00'
+	When today is '2020-11-26 14:41'
+	Then the baby is '100' days old
---
+++ BabyAgeApp/BabyAgeApp.Specs/Steps/BabySteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BabyAgeApp.Specs.Drivers;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace BabyAgeApp.Specs.Steps
+{
+    [Binding]
+    public class BabySteps
+    {
+        private readonly AppiumDriver _appiumDriver;
+
+        public BabySteps(AppiumDriver appiumDriver)
+        {
+            _appiumDriver = appiumDriver;
+        }
+
+        [Given(@"the baby is born on '(.*)'")]
+        public void GivenTheBabyIsBornOn(string babyBirthday)
+        {
+            
+        }
+
+        [When(@"today is '(.*)'")]
+        public void WhenTodayIs(string today)
+        {
+            
+        }
+
+        [Then(@"the baby is '(.*)' days old")]
+        public void ThenTheBabyIsDaysOld(int daysOld)
+        {
+            var ageInDaysElement = _appiumDriver.Driver.FindElementByAccessibilityId("ageInDays");
+
+            var actualDaysOld = int.Parse(ageInDaysElement.Text);
+            actualDaysOld.Should().Be(daysOld);
+        }
+
+    }
+}
