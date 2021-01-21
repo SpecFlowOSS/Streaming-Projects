f7d2d6019991b36f9e9534314135e60c7ebd29ca
Andreas Willich <andreas@willich.io>
2021-01-21 14:00:29 +0100

    working empty generator plugin

---
+++ StreamingPlugin/StreamingGeneratorPlugin.SpecFlowPlugin/StreamingGeneratorPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly: GeneratorPlugin(typeof(StreamingGeneratorPlugin.SpecFlowPlugin.StreamingGeneratorPlugin))]
+
+namespace StreamingGeneratorPlugin.SpecFlowPlugin
+{
+    class StreamingGeneratorPlugin : IGeneratorPlugin
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters,
+            UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            
+        }
+    }
+}
