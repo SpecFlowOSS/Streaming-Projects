452b88d5ac6f8ccc48cb003a006c73e30e548f2f
Andreas Willich <andreas@willich.io>
2021-01-21 13:38:29 +0100

    add example how to react on hook like events

--- StreamingPlugin/StreamingRuntimePlugin.SpecFlowPlugin/StreamingRuntimePlugin.cs
+++ StreamingPlugin/StreamingRuntimePlugin.SpecFlowPlugin/StreamingRuntimePlugin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Plugins;
 using TechTalk.SpecFlow.UnitTestProvider;
 
@@ -13,7 +14,26 @@ namespace StreamingRuntimePlugin.SpecFlowPlugin
         public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters,
             UnitTestProviderConfiguration unitTestProviderConfiguration)
         {
-            
+            runtimePluginEvents.CustomizeGlobalDependencies += RuntimePluginEvents_CustomizeGlobalDependencies;
         }
+
+        private void RuntimePluginEvents_CustomizeGlobalDependencies(object sender, CustomizeGlobalDependenciesEventArgs e)
+        {
+            var runtimePluginTestExecutionLifecycleEvents = e.ObjectContainer.Resolve<RuntimePluginTestExecutionLifecycleEvents>();
+
+            runtimePluginTestExecutionLifecycleEvents.AfterStep += RuntimePluginTestExecutionLifecycleEvents_AfterStep;
+        }
+
+        private void RuntimePluginTestExecutionLifecycleEvents_AfterStep(object sender, RuntimePluginAfterStepEventArgs e)
+        {
+            var scenarioContext = e.ObjectContainer.Resolve<ScenarioContext>();
+
+            if (scenarioContext.ScenarioExecutionStatus != ScenarioExecutionStatus.OK)
+            {
+                //we have an error in the step
+            }
+        }
+
+
     }
 }
